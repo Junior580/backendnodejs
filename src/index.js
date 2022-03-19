34b3db85b23ee6a0ef07e8e9eba4a86305e9f75b
Junior580 <junior_silvapereira@hotmail.com>
2022-03-18 22:19:15 -0300

    first commit

---
+++ src/index.js
@@ -0,0 +1,49 @@
+const express = require('express');
+
+const app = express();
+
+app.use(express.json());
+
+app.get('/projects', (request, response) => {
+  const { title, owner } = request.query;
+  console.log(title);
+  console.log(owner);
+
+  return response.json([
+    'Projeto 1',
+    'Projeto 2',
+  ]);
+});
+
+app.post('/projects', (request, response) => {
+
+  const { title, owner } = request.body;
+
+  console.log(title);
+  console.log(owner);
+
+  return response.json([
+    'Projeto 3',
+    'Projeto 4',
+  ]);
+})
+
+app.put('/projects/:id', (request, response) => {
+  const params = request.params;
+  console.log(params);
+  return response.json([
+    'Projeto 3',
+    'Projeto 5',
+  ]);
+})
+
+app.put('/projects/:id', (request, response) => {
+  return response.json([
+    'Projeto 3',
+    'Projeto 4',
+  ]);
+})
+
+app.listen(3333, () => {
+  console.log('🚀 Back-end started!');
+});
